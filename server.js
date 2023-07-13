a20a1d980c872349978c47283a72953b69e9e8f8
Scarlethev <scarleth_07@hotmail.com>
2023-07-13 18:45:27 -0300

    Cambios

--- server.js
+++ server.js
@@ -2,35 +2,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
 require("./Config/database")
-require ("./Config/databaseItinerary")
 
 const app = express();
 
-const appItinerary = express();
-
-const RouterItinerary= require ("./Routes/routesItinerary")
-const PORTitinerary =process.env.PORT || 6000;
 
 const Router= require("./Routes/routes")
 const PORT = process.env.PORT || 5000;
 
 console.log(PORT)
-console.log(PORTitinerary)
 
-
-//ITINERARY
-
-appItinerary .use(cors())
-appItinerary .set("portItinerary",PORTitinerary);
-
-appItinerary .use(express.json())
-appItinerary .use("/Api", RouterItinerary)
-
-appItinerary.listen(PORTitinerary,()=> {
-    console.log("SERVIDOR ITINERARY CORRIENDO EN PUERTO "+ appItinerary.get("portItinerary"))
-})
-
-//CITIES
 app.use(cors())
 app.set("port",PORT);
 
